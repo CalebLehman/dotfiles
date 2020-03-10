9bf9518b850c05bedc3c8937014c04f9ac0c1c4d
Caleb Lehman <lehman.346@osu.edu>
2020-03-10 17:52:39 -0400

    Remove redundant fish binding
    The binding of `Ctrl + C` to clear a line is unnecessary. It seems to
    work as expected "out-of-the-box" on some systems. On others, switching
    to `normal` mode and deleting the line with `dd` works as a substitute.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,6 @@ set -g -x fish_greeting ''
 function custom_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
-  bind -M insert \cc kill-whole-line force-repaint
 end
 set -g fish_key_bindings custom_bindings
 
