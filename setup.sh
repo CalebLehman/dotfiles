7e62efc765411f795c27a57a600339ab8cb98461
CalebLehman <38224343+CalebLehman@users.noreply.github.com>
2020-03-11 14:49:59 -0400

    Change setup.sh to install termite last

--- setup.sh
+++ setup.sh
@@ -6,44 +6,10 @@
 # I would mostly be worried about GNU stow not working
 # if config files already existed
 
-### Terminal Emulator ###
-
-# I don't have any strong allegiances to any terminal emulator, but I've
-# used [Termite](https://github.com/thestinger/termite) and have been
-# happy with it.
-# I usually install it as follows (taken from https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh):
-
 sudo add-apt-repository -y universe
 sudo add-apt-repository -y ppa:dawidd0811/neofetch # necessary on some older releases
 sudo apt-get update -y
-sudo apt-get install -y \
-git \
-g++ \
-libgtk-3-dev \
-gtk-doc-tools \
-gnutls-bin \
-valac \
-intltool \
-libpcre2-dev \
-libglib3.0-cil-dev \
-libgnutls28-dev \
-libgirepository1.0-dev \
-libxml2-utils \
-gperf \
-neofetch \
-curl
-
-mkdir ~/temp && cd ~/temp
-
-git clone https://github.com/thestinger/vte-ng.git
-cd vte-ng && ./autogen.sh && make && sudo make install && cd ..
-
-git clone --recursive https://github.com/thestinger/termite.git
-cd termite && make && sudo make install && cd .. && rm temp -rf
-
-sudo ldconfig
-
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
+sude apt-get install -y curl neofetch
 
 ### Configuration Files ###
 
@@ -99,3 +65,37 @@ fish -c updatevim
 
 sudo apt-get install -y tmux
 stow tmux
+
+### Terminal Emulator ###
+
+# I don't have any strong allegiances to any terminal emulator, but I've
+# used [Termite](https://github.com/thestinger/termite) and have been
+# happy with it.
+# I usually install it as follows (taken from https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh):
+
+sudo apt-get install -y \
+git \
+g++ \
+libgtk-3-dev \
+gtk-doc-tools \
+gnutls-bin \
+valac \
+intltool \
+libpcre2-dev \
+libglib3.0-cil-dev \
+libgnutls28-dev \
+libgirepository1.0-dev \
+libxml2-utils \
+gperf \
+
+mkdir ~/temp && cd ~/temp
+
+git clone https://github.com/thestinger/vte-ng.git
+cd vte-ng && ./autogen.sh && make && sudo make install && cd ..
+
+git clone --recursive https://github.com/thestinger/termite.git
+cd termite && make && sudo make install && cd .. && rm temp -rf
+
+sudo ldconfig
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
