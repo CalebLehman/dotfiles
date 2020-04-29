67e8439eb755e504e73bebaa752c2b47fa749484
CalebLehman <lehman.346@osu.edu>
2020-04-29 10:17:46 -0400

    Set Ctrl-C to clear line for newer fish versions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ set -g -x fish_greeting ''
 function custom_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
+  bind -M insert \cc 'commandline ""'
 end
 set -g fish_key_bindings custom_bindings
 
