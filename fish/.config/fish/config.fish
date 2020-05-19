dbb9f406fd947f5cf98c4fc8b2cdb7018ccd2af0
CalebLehman <lehman.346@osu.edu>
2020-05-19 11:03:55 -0400

    Add conda check in fish configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,11 @@ set -g fish_key_bindings custom_bindings
 if type -q neofetch
   neofetch
 end
+
+if type -q conda
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/clehman/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+end
+
