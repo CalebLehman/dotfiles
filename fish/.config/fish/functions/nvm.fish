7da42df1179f112e5147e3571b509973c68988b4
CalebLehman <lehman.346@osu.edu>
2021-03-04 23:20:38 -0500

    Fix path to nvm install

--- fish/.config/fish/functions/nvm.fish
+++ fish/.config/fish/functions/nvm.fish
@@ -1,8 +1,8 @@
 function nvm
-  if test -f ~/.nvm/nvm.sh
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+  if test -f ~/.config/nvm/nvm.sh
+    bass source ~/.config/nvm/nvm.sh --no-use ';' nvm $argv
   else
-    echo unable to find ~/.nvm/nvm.sh
+    echo unable to find ~/.config/nvm/nvm.sh
     return 1
   end
 end
